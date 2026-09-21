24b76600ed85c322d5d5bbe47d9c0c2c5477b277
ifilex <ifilex@gmail.com>
2026-09-20 22:03:29 -0300

    Update apps-data.js

--- apps-data.js
+++ apps-data.js
@@ -289,7 +289,7 @@ window.appData = [
         "ja": "Pixel Leapは、プロシージャル生成されるレベルを備えた2Dプラットフォーマーです。つまり、プレイするたびにユニークで無限の挑戦が楽しめます。タッチ操作とカスタマイズ可能なコントロール、アクションに合わせて変化するダイナミックなサウンドトラック、どのデバイスでも続けられるクラウドセーブ、そして世界中のプレイヤーと競い合えるグローバルランキングをお楽しみください。"
     },
     "category": "games",
-    "link": "https://ifilex.github.io/WineBOX-Store/pixelleap/index.html"
+    "link": "https://ifilex.github.io/WineBOX-Store/webapps/pixelleap/index.html"
 },
 {
     "name": "PixelPou",
@@ -306,7 +306,7 @@ window.appData = [
         "ja": "PixelPouは、Pou風のレトロなバーチャルペットゲームで、ピクセルアートのグラフィックを備えています。毎日の世話（食事、衛生、睡眠、遊び）を行ってペットを育てる、子ども向けに設計されたゲームです。教育的なミニゲーム、カスタマイズオプション、共有可能なペットの写真撮影機能が含まれています。"
     },
     "category": "games",
-    "link": "https://ifilex.github.io/WineBOX-Store/pixelpou/index.html"
+    "link": "https://ifilex.github.io/WineBOX-Store/webapps/pixelpou/index.html"
 },
 {
   "name": "Retrocraft",
